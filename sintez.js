f133a5684157821fc3a1244c84e9ceabc3455086
darthspectrum <muradianator@gmail.com>
2025-05-11 01:48:21 +0400

    :construction: setup for step 03

--- sintez.js
+++ sintez.js
@@ -1,4 +1,4 @@
-export { encodeWAV, generatePCM, tokenize, typeify };
+export { encodeWAV, evaluate, generatePCM, tokenize, typeify };
 
 // sample[n]= A ⋅ sin(2 * π * f * (n / R)​)
 
@@ -74,8 +74,7 @@ const typify = (name) => {
 };
 
 const typeify = (token) => {
-  const number = Number.parseFloat(token, 10);
-  return Number.isNaN(number) ? atom(token) : number;
+  throw new Error("Not implemented");
 };
 
 const tokenize = (input) => {
@@ -108,14 +107,4 @@ const tokenize = (input) => {
   };
 
   return loop(input, "", []);
-};
-
-const run = (
-  program,
-  definitions = [],
-) => {
-  const tokens = tokenize(program);
-  const value = evaluate(tokens, definitions);
-
-  return value;
 };
